5ad1aea5ee5edbb9e181b535b1bb5eb2757a556e
Cesar Santos MSI <cesar.santos@pucp.edu.pe>
2021-11-26 19:45:06 -0500

    Commit de Prueba: Bean BPrueba

---
+++ src/main/java/pe/edu/pucp/pteleshock/Beans/BPrueba.java
@@ -0,0 +1,122 @@
+package pe.edu.pucp.pteleshock.Beans;
+
+public class BPrueba {
+
+    private int idusuario;
+    private int idfarmacia;
+    private String nombre;
+    private String ruc;
+    private String direccion;
+    private String correo;
+    private String estado;
+    private int pedEntregados;
+    private int pedPendientes;
+    private int pedCancelados;
+    private int pedDeseados;
+
+    public BPrueba() {
+    }
+
+    public BPrueba(int idusuario, int idfarmacia, String nombre, String ruc, String direccion, String correo, String estado, int pedEntregados, int pedPendientes, int pedCancelados, int pedDeseados) {
+        this.idusuario = idusuario;
+        this.idfarmacia = idfarmacia;
+        this.nombre = nombre;
+        this.ruc = ruc;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.estado = estado;
+        this.pedEntregados = pedEntregados;
+        this.pedPendientes = pedPendientes;
+        this.pedCancelados = pedCancelados;
+        this.pedDeseados = pedDeseados;
+    }
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public int getIdfarmacia() {
+        return idfarmacia;
+    }
+
+    public void setIdfarmacia(int idfarmacia) {
+        this.idfarmacia = idfarmacia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getPedEntregados() {
+        return pedEntregados;
+    }
+
+    public void setPedEntregados(int pedEntregados) {
+        this.pedEntregados = pedEntregados;
+    }
+
+    public int getPedPendientes() {
+        return pedPendientes;
+    }
+
+    public void setPedPendientes(int pedPendientes) {
+        this.pedPendientes = pedPendientes;
+    }
+
+    public int getPedCancelados() {
+        return pedCancelados;
+    }
+
+    public void setPedCancelados(int pedCancelados) {
+        this.pedCancelados = pedCancelados;
+    }
+
+    public int getPedDeseados() {
+        return pedDeseados;
+    }
+
+    public void setPedDeseados(int pedDeseados) {
+        this.pedDeseados = pedDeseados;
+    }
+}
+
